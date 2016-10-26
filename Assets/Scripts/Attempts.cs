697d7b0857867ac1aab3f43ce3c2a95f08b2d757
erinf <erincfink@gmail.com>
2016-10-26 15:02:06 -0500

    Attempts And Power Slider

---
+++ Assets/Scripts/Attempts.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Attempts : MonoBehaviour {
+
+    private int attempts = 0;
+
+    // Call when there is another attempt
+    public void Attempted() {
+        attempts++;
+    }
+
+    void Update() {
+        gameObject.GetComponent<Text>().text = " Attempts: " + attempts;
+    }
+}
