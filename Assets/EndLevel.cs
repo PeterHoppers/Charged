685cd3353c3f0ae184004d171f72cb0447ba71a9
peck2703 <mrpeckjr@gmail.com>
2016-10-26 14:49:23 -0500

    EndPoint and End Level
    EndPoint is a prefab childed to a canvas object and EndLevel is the
    script that loads it.

---
+++ Assets/EndLevel.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EndLevel : MonoBehaviour
+{
+    //Grab the PlayerGo using the tag in the Start()
+    GameObject playerGO;
+
+    //Load the scene into the object then upon trigger, load next scene
+    public Scene NextLevel;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerGO = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == playerGO)
+        {
+            SceneManager.LoadScene(NextLevel.ToString());
+        }
+    }
+}
