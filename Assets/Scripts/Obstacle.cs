7249e0a524d330d55de6ba3f02126139d3bebdef
Borgeshc <Borgeshc@gmatc.matc.edu>
2016-10-26 14:51:06 -0500

    Obstacles and ScoreManager
    Added score manager
    Added obstacles

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour
+{
+    ScoreManager scoreManager;                                                          //References the scoreManager
+
+    void Start()
+    {
+        scoreManager = GameObject.Find("GameManager").GetComponent<ScoreManager>();     //References the scoreManager
+    }
+
+    void OnTriggerEnter2D(Collider2D other)                                             //Checks for collision with the player
+    {
+        if(other.tag == "Player")   
+        {
+            Destroy(other.gameObject);                                                             //Destroyes the player and updates the score
+            scoreManager.UpdateScore();
+        }
+    }
+}
