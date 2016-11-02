3b9a2fab6c65c55b87cd586f1c8252a2ec4bb353
peck2703 <mrpeckjr@gmail.com>
2016-11-02 16:43:31 -0500

    Still trying to fix the DB

--- Assets/Scripts/DatabaseParser.cs
+++ Assets/Scripts/DatabaseParser.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Mono.Data.SqliteClient;
 using UnityEngine;
 using System.Data;
+using System.Data.SqlClient;
 
 class DatabaseParser : MonoBehaviour
 {
     void Start()
     {
-        string conn = "URI=file:" + Application.dataPath + "ChargedDB.db";              //Full path to the Database
+        string connString = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = C:\\Users\\Peck2703\\Documents\\GitHub\\Charged\\Assets\\ChargedTest.mdf; Integrated Security = True; Connect Timeout = 30";
+        SqlConnection myConn = null;
+        SqlDataReader rder = null;
+        
+
+        try
+        {
+            myConn = new SqlConnection(connString);
+            myConn.Open();
+
+            //SqlCommand cmd = new SqlCommand();
+
+            SqlCommand myCommand = new SqlCommand("select * from dbo.Users", myConn);
+
+            
+        }
+        catch(Exception ex)
+        {
+            Debug.Log(ex.ToString());
+        }
+        /*string conn = "URI=file:" + Application.dataPath + "ChargedDB.db";              //Full path to the Database
         IDbConnection dbConn;
 
         dbConn = (IDbConnection) new SqliteConnection(conn);
@@ -39,5 +59,6 @@ class DatabaseParser : MonoBehaviour
 
         dbConn.Close();
         dbConn = null;
+        */
     }
 }
