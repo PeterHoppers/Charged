980e5755fcf5bbba5956ab889abd1ced70ae1643
peck2703 <mrpeckjr@gmail.com>
2016-11-02 11:31:50 -0500

    DatabaseTools in the Drive

---
+++ Assets/Scripts/DatabaseParser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mono.Data.SqliteClient;
+using UnityEngine;
+using System.Data;
+
+class DatabaseParser : MonoBehaviour
+{
+    void Start()
+    {
+        string conn = "URI=file:" + Application.dataPath + "ChargedDB.db";              //Full path to the Database
+        IDbConnection dbConn;
+
+        dbConn = (IDbConnection) new SqliteConnection(conn);
+
+        dbConn.Open();
+        IDbCommand dbComd = dbConn.CreateCommand();
+
+        string sQuery = "SELECT ID, Username " + "FROM UserAccounts";
+
+        dbComd.CommandText = sQuery;
+
+        IDataReader reader = dbComd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            int readID = reader.GetInt32(0);
+            string readUser = reader.GetString(1);
+
+            Debug.Log("ID: " + readID + " and UserName: " + readUser);
+        }
+        reader.Close();
+        reader = null;
+
+        dbComd.Dispose();
+        dbComd = null;
+
+        dbConn.Close();
+        dbConn = null;
+    }
+}
