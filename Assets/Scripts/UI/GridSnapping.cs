bd35b1eef369196b763b7e0a919a8d90b5b98481
PeterHoppers <hoppepj@gmatc.matc.edu>
2016-11-04 17:52:11 -0500

    Added Slider so player can change the senstivey

--- Assets/Scripts/UI/GridSnapping.cs
+++ Assets/Scripts/UI/GridSnapping.cs
@@ -4,19 +4,18 @@ using System.Collections;
 
 public class GridSnapping : MonoBehaviour {
 
-    [Tooltip("Object will snap to multiples of this number")]
-    public float snapTo = 1;
-    [Tooltip("When clicked, it will only snap on start. Else, it will snap on update as well")]
-    public bool onlyStart = true;
     RectTransform currentTransfrom;
+    GridManager gridSystem;
 
     // Use this for initialization
     void Start ()
     {
+        gridSystem = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GridManager>();
+
         if (GetComponent<RectTransform>() != null)
         {
             currentTransfrom = GetComponent<RectTransform>();
-            snap();
+            currentTransfrom.localPosition = gridSystem.snap(currentTransfrom.localPosition);
         }
         else
         {
@@ -24,31 +23,14 @@ public class GridSnapping : MonoBehaviour {
         }
 	}
 
-    void snap()
-    {
-        float xPos = currentTransfrom.localPosition.x;
-        float yPos = currentTransfrom.localPosition.y;
-        xPos = findSnap(xPos);
-        yPos = findSnap(yPos);
-        currentTransfrom.localPosition = new Vector2(xPos, yPos);
-    }
-
-    float findSnap(float number)
-    {
-        if (snapTo == 0) return number;         //cannot divide by 0
-
-        number = (number * (1 / snapTo));       //math equation to
-        number = Mathf.Round(number);           //round to nearest number
-        number = (number / (1 / snapTo));       //that is a multiple of snapTo
-        return number;
-    }
+    
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (!onlyStart)
+        if (!gridSystem.onlyStart)
         {
-            snap();
+            currentTransfrom.localPosition = gridSystem.snap(currentTransfrom.localPosition);
         }
 	}
 }
