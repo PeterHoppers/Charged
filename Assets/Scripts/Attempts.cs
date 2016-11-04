c5065385f86efbbf1f41efe4ef5b31bcf0715524
Koesterc <Koesterc@gmatc.matc.edu>
2016-11-04 12:21:41 -0500

    updated and fixed level selection
    added arrow/button scrolling
    fixed code (cuz sam was being  a jerk)
    did some minor touch ups

---
+++ Assets/Scripts/Attempts.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Attempts : MonoBehaviour {
+
+    private int attempts = 0;
+
+    // Call when there is another attempt
+    public void Attempted() {
+        attempts++;                         // Call to increase attempt
+        gameObject.GetComponent<Text>().text = " Attempts: " + attempts;    // Update Attempt text
+    }
+}
---
+++ Assets/Scripts/StartMenus/LevelSelect.cs
@@ -0,0 +1,94 @@
+﻿//Chris - Mike > Sam^2
+//buhahaha
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class LevelSelect : MonoBehaviour
+{
+    [Header("Game Object Levels")]
+    GameObject level;
+    public bool[] locked;
+    [HideInInspector]
+    public int sceneLevel = 0;
+    float doubleClick;
+    public AudioSource error;
+    int levelCount = 0;
+    int xPos = 0;
+    public GameObject content;
+    GameObject myText;
+    GameObject[] levels;
+
+
+
+    // setting this object's activeness, 
+    void OnEnable()
+    {
+        levelCount = 0;
+
+        //  loading prefabs from the (resource folder > Levels) and assigning them to the variable "levels"
+        levels = Resources.LoadAll<GameObject>("Levels");
+        //looping through all the levels from the (resource folder > Levels) and instantiating them as GameObjects
+         foreach (GameObject go in levels)
+        {
+            level = Instantiate(go);
+            CreateSlot();
+            levelCount++;
+        }
+    }
+    //settig this object's activeness to false will destroy all levels in order to prevent duplication
+    //the reason we istantiate OnEnable is so that we can keep the information up to date
+    void OnDisable()
+    {
+        foreach (Transform child in content.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+
+    public void SelectLevel(int myLevel)
+    {
+        sceneLevel = myLevel;
+    }
+
+    public void PlayLevel(string myScene)
+    {
+        if (doubleClick <= Time.time)
+            doubleClick = Time.time + .2f;
+        // checking to see if the level image was double clicked (CUZ DOUBLE CLICKING IS BETTER THAN SINGLE CLICKING!)
+        else
+        {
+            //if so, play scene level
+            if (!locked[sceneLevel])
+                SceneManager.LoadScene(myScene);
+       //     else
+              //  error.GetComponent<AudioSource>().Play(); //error plays because the player attempted to double click a locked level. Lol Noobs will try anything. 
+        }
+    }
+
+    private void CreateSlot()
+    {
+        //creating the level image slot
+        level.name = "Level " + levelCount.ToString();   //turning level's name and the level number to a string
+        level.GetComponentInChildren<Text>().text = level.name;   //finding the child text of that level and printing the level name and level number
+        level.transform.SetParent(content.transform);  //assigning the instantiate level to child of the GameObject, "content"
+        level.GetComponent<RectTransform>().localPosition = new Vector3(xPos, 0, 0);   //here we are assigning the proper scale, rotation, position, width, and height of the gameobject "level"
+        level.GetComponent<RectTransform>().localRotation = Quaternion.identity;
+        level.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+        xPos += (int)level.GetComponent<RectTransform>().rect.height;
+    }
+    //scrolling by clicking the left arrow
+    public void LeftArrowScroll()
+    {
+        content.transform.position += new Vector3(+50, 0, 0);
+    }
+    //scrolling by clicking the right arrow
+    public void RightArrowScroll()
+    {
+        content.transform.position -= new Vector3(+50, 0, 0);
+    }
+
+}
