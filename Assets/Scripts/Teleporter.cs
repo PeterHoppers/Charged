ec922a3123b9816fb772c87cb42954d482bc09ae
PeterHoppers <hoppepj@gmatc.matc.edu>
2016-11-09 11:34:37 -0600

    Teleporter Added

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+
+    [Tooltip("Where you want the teleporter to go")]
+    public Transform newPosition;
+
+    void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.tag.Equals("Player"))
+        {
+            obj.transform.position = newPosition.position;
+        }
+    }
+}
+
+//~~~~Peter
