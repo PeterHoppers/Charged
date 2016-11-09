66734d4502380216098aa0e7d381b3bc2f7c4d7a
PeterHoppers <hoppepj@gmatc.matc.edu>
2016-11-09 14:20:57 -0600

    Placing Components Added

--- Assets/Scripts/UI/LevelEditorScript.cs
+++ Assets/Scripts/UI/LevelEditorScript.cs
@@ -6,11 +6,19 @@ public class LevelEditorScript : MonoBehaviour {
     public static GameObject background;
     static GameObject levelBackground;
     public GameObject backgroundPrefab;
+    GameObject folder;
+
+
     NecessarySpawns necessarySpawns;
     void Start()
     {
-        FindBackground();
         necessarySpawns = GameObject.Find("GameManager").GetComponent<NecessarySpawns>();
+        folder = GameObject.Find("CloneFolder");
+
+        if (folder == null)
+        {
+            Debug.LogError("There is not an object called \"CloneFolder\"");
+        }
     }
 
     public void SetBackground()
@@ -30,5 +38,19 @@ public class LevelEditorScript : MonoBehaviour {
         levelBackground = GameObject.Find("Background");
         if (!levelBackground)
             Debug.LogError("No level image componenet was found");
+    }
+
+    public void CreateNewObject()
+    {
+        GameObject clone = Instantiate(gameObject, folder.transform) as GameObject;
+        clone.AddComponent<Draggable>();                                     //allows for dragging
+        clone.AddComponent<GridSnapping>();                                 //allows for grid snapping
+        clone.tag = "Clone";                    
+        clone.GetComponent<Button>().enabled = false;                       //prevent it from being clicked
+        clone.GetComponent<RectTransform>().localPosition = Vector2.zero;   //spawns in the middle
+        BoxCollider2D boxC = clone.AddComponent<BoxCollider2D>();           //adds a collider for trash
+        boxC.isTrigger = true;                                              
+        boxC.size = new Vector2(100, 100);
+
     }
 }
---
+++ Assets/Scripts/UI/TrashCan.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrashCan : MonoBehaviour {
+
+    Transform cloneFolder;
+
+    void Start()
+    {
+        cloneFolder = GameObject.Find("CloneFolder").transform;  //finds where all the items are stored
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag.Equals("Clone"))
+        {
+            Destroy(col.gameObject);
+        }
+    }
+
+    public void DeleteAll()
+    {
+        foreach (Transform child in cloneFolder)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+}
