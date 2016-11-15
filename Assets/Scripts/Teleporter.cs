48695587b4342781bbd21ee8b3e1fdbb3c6ad3b3
Borgeshc <Borgeshc@gmatc.matc.edu>
2016-11-15 10:20:19 -0600

    Merged teleporter into dev

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+
+    [Tooltip("Where you want the teleporter to go")]
+    public Transform newPosition;
+
+    void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.tag.Equals("PlayerOneProjectile") || obj.tag.Equals("PlayerTwoProjectile"))
+        {
+            obj.transform.position = newPosition.position;
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
+
+//~~~~No Fun Peter
