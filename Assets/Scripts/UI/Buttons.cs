aed1071003c33107544d99c43b82e3be632bb7a3
PeterHoppers <hoppepj@gmatc.matc.edu>
2016-11-28 14:49:26 -0600

    Added Comments to Code that Weren't Merged

--- Assets/Scripts/UI/Buttons.cs
+++ Assets/Scripts/UI/Buttons.cs
@@ -4,13 +4,9 @@ using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
-    public static int nextLevel = 2;
     public void NextLevel()
     {
-        
-        //This will require a sceneManager.
-        SceneManager.LoadScene(nextLevel);
-        nextLevel++;
+        BasicUtilities.NextLevel();
     }
 
     public void MainMenu()
--- Assets/Scripts/UI/LevelEditorScript.cs
+++ Assets/Scripts/UI/LevelEditorScript.cs
@@ -57,6 +57,18 @@ public class LevelEditorScript : MonoBehaviour {
         boxC.size = new Vector2(100, 100);
     }
 
+    public GameObject CreateNewObjectAtCursor()
+    {
+        GameObject clone = Instantiate(gameObject, folder.transform) as GameObject;
+        clone.AddComponent<GridSnapping>();                                             //allows for grid snapping
+        clone.GetComponent<Button>().enabled = false;                                   //prevent it from being clicked
+        clone.GetComponent<RectTransform>().localPosition = CursorPosition();           //spawns at the cursor
+        BoxCollider2D boxC = clone.AddComponent<BoxCollider2D>();                       //adds a collider for trash
+        boxC.isTrigger = true;
+        boxC.size = new Vector2(100, 100);
+        return clone;
+    }
+
     public GameObject CreateNewObjectAtCursor(GameObject aGameObject)
     {
         GameObject clone = Instantiate(aGameObject, folder.transform) as GameObject;
@@ -69,15 +81,13 @@ public class LevelEditorScript : MonoBehaviour {
         return clone;
     }
 
-    public GameObject CreateNewObjectAtCursor()
+    public GameObject CreateNewObjectAtCursor(GameObject aGameObject, string tag)
     {
-        GameObject clone = Instantiate(gameObject, folder.transform) as GameObject;
+        GameObject clone = Instantiate(aGameObject, folder.transform) as GameObject;
+        clone.GetComponent<RectTransform>().localScale = Vector3.one;
         clone.AddComponent<GridSnapping>();                                             //allows for grid snapping
-        clone.GetComponent<Button>().enabled = false;                                   //prevent it from being clicked
         clone.GetComponent<RectTransform>().localPosition = CursorPosition();           //spawns at the cursor
-        BoxCollider2D boxC = clone.AddComponent<BoxCollider2D>();                       //adds a collider for trash
-        boxC.isTrigger = true;
-        boxC.size = new Vector2(100, 100);
+        clone.tag = tag;
         return clone;
     }
 
