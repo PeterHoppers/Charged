ee54ef2a2518e081cd1e8e68b57e6d70854c9f3a
Borgeshc <Borgeshc@gmatc.matc.edu>
2016-11-30 15:37:01 -0600

    Merged pause menu

---
+++ Assets/ExitLevelScript.cs
@@ -0,0 +1,85 @@
+﻿//Chritopher J. Koester
+//preschool homework assignment 101
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ExitLevelScript : MonoBehaviour {
+    GameObject pauseMenu;
+    GameObject doubleCheck;
+    GameObject menu;
+    bool wasEnabled = false;//this bool checks to see if the ionplacement was already on. That way, we don't enable it if it wasn't
+    //already enabled before the player paused the game
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        //finding el menu
+        pauseMenu = GameObject.Find("Canvas/PauseMenu/PauseBox");
+        menu = GameObject.Find("Canvas/PauseMenu");
+        doubleCheck = GameObject.Find("Canvas/PauseMenu/DoubleCheckBox");
+        doubleCheck.SetActive(false);
+        menu.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //disabling and enabling the pause menu depending on whether or not it is active or false
+        if (Input.GetKeyDown("escape"))
+        {
+            if (!menu.activeSelf)
+            {//pausing game
+                if (gameObject.GetComponent<IonPlacement>().enabled == true)
+                {//recalling if the game object was last enabled
+                    gameObject.GetComponent<IonPlacement>().enabled = false;
+                    wasEnabled = true;
+                }
+                menu.SetActive(true);
+                Time.timeScale = 0;
+            }
+            else
+            {//resuming game
+                if (wasEnabled)
+                {
+                    gameObject.GetComponent<IonPlacement>().enabled = true;
+                    wasEnabled = false;
+                }
+                menu.SetActive(false);
+                Time.timeScale = 1;
+            }
+ 
+        }
+	}
+
+    public void DoubleCheck()
+    {
+        doubleCheck.SetActive(true);
+        pauseMenu.SetActive(false);
+    }
+    public void Cancel()
+    {
+        doubleCheck.SetActive(false);
+        pauseMenu.SetActive(true);
+    }
+
+    public void MainMenu()
+    {//returning to the menu
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+
+    }
+
+    public void Resume()
+    {//resuming the game
+        if (wasEnabled)
+        {//recalling if the game object was last enabled
+            gameObject.GetComponent<IonPlacement>().enabled = true;
+            wasEnabled = false;
+        }
+        menu.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
--- Assets/Scripts/Gameplay/PlayerManager.cs
+++ Assets/Scripts/Gameplay/PlayerManager.cs
@@ -15,7 +15,6 @@ public class PlayerManager : MonoBehaviour
     GameObject canvas;
     void Start()
     {
-        print(p1Clone);
         canvas = GameObject.Find("Canvas");
         startPoint = GameObject.Find("StartPoint");
         switch (numberOfPlayers)            //If one player spawn p1, if two spawn p1 and p2
