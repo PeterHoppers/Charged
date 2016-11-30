fc3e47044a61245d54c2cdb6bd56525fe7d394ec
Borgeshc <Borgeshc@gmatc.matc.edu>
2016-11-30 15:56:58 -0600

    Began the merging process

--- Assets/Scripts/Gameplay/ScoreManager.cs
+++ Assets/Scripts/Gameplay/ScoreManager.cs
@@ -6,7 +6,9 @@ public class ScoreManager : MonoBehaviour
 {
     [Tooltip("The canvas text object for the tries count.")]
     public Text triesText;
-    int tries;                                  //Keeps track of the number of tries.
+    [HideInInspector]//Hice pública Esto! fue modificado por Cristóbal así puedo obtener esta información desde otro script
+    public int tries;                                  //Keeps track of the number of tries.
+    [HideInInspector]
     public GameObject gameManager;
     public GameObject levelCompletedPanel;
     public GameObject nextLevelButton;
@@ -15,14 +17,18 @@ public class ScoreManager : MonoBehaviour
     public GameObject star2;
     public GameObject star3;
     int starsNeededToCont;
-    public int oneStarAttempts;
-    public int twoStarAttempts;
-    public int threeStarAttempts;
+    public int oneStarScore;
+    public int twoStarScore;
+    public int threeStarScore;
     int starsEarned;
     public static bool canContinue = false;
+    public IonTrackerScript ionTracker; //getting the script
+    public IonPlacement ionPlacement; //getting the script
 
     void Start()
     {
+        ionTracker = GameObject.Find("Canvas/IonTrackers").GetComponent<IonTrackerScript>();
+        ionPlacement = GameObject.Find("GameManager").GetComponent<IonPlacement>();
         gameManager = GameObject.Find("GameManager");
         if (gameManager == null)
             Debug.LogError("No game manager found");
@@ -43,20 +49,26 @@ public class ScoreManager : MonoBehaviour
     {
         tries++;                                //Updates the try count
         triesText.text = "Attempts: " + tries;
+        ionTracker.ScoreTracker(); //Refrescante la puntuación en el IonTrackerScript
     }
     public void ResetScore()
     {
         tries = 0;
         triesText.text = "Attempts: " + tries;      //Resets the try count
+        ionTracker.ScoreTracker(); //Refrescante la puntuación en el IonTrackerScript
     }
     public void LevelCompleted()
     {
+        ionTracker.points += 100 + (((int)ionPlacement.availablePositiveIons + (int)ionPlacement.availableNegativeIons) * 25) - (((int)IonPlacement.numberOfPositives + (int)IonPlacement.numberOfNegatives) * 25);
+        ionTracker.ScoreTracker();
+        //using this temp variable as a means to compare the actual score to a percentage of to its max (otherwise the statement would, again, be long)
+        float temp = 100 + (((int)ionPlacement.availablePositiveIons + (int)ionPlacement.availableNegativeIons) * 25);
         print("Level completed called");
         if (gameManager != null)
             gameManager.GetComponent<IonPlacement>().enabled = false;
 
         levelCompletedPanel.SetActive(true);
-        if (tries <= oneStarAttempts)
+        if (ionTracker.points >= threeStarScore)
         {
             print("3 stars earned");
             star1.SetActive(true);
@@ -64,16 +76,16 @@ public class ScoreManager : MonoBehaviour
             star3.SetActive(true);
             starsEarned = 3;
         }
-        else if (tries <= twoStarAttempts && tries >= oneStarAttempts)
+        else if (ionTracker.points < threeStarScore && ionTracker.points >= twoStarScore)
         {
             print("2 stars earned");
             star1.SetActive(true);
             star2.SetActive(true);
             starsEarned = 2;
         }
-        else if (tries <= threeStarAttempts && tries >= twoStarAttempts)
+        else if (ionTracker.points < twoStarScore && ionTracker.points >= oneStarScore)
         {
-            print("2 stars earned");
+            print("1 stars earned");
             star1.SetActive(true);
             starsEarned = 1;
         }
