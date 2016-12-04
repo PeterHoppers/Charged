5ef00ef54117c5b722e189fdeb7c1b0c0cc84a0a
PeterHoppers <hoppepj@gmatc.matc.edu>
2016-12-04 12:56:35 -0600

    Started Fade tests

---
+++ Assets/Scripts/UI/TutorialText.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TutorialText : MonoBehaviour {
+
+    public KeyCode[] keys;
+    public float delay;
+    Text text;
+    double aplpha = 255;
+    // Use this for initialization
+    void Start ()
+    {
+        text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (BasicUtilities.onlyOnce("FadeIn"))
+        {
+            StartCoroutine(FadeIn());
+        }
+        foreach (KeyCode key in keys)
+        {
+            if (Input.GetKey(key))
+            {
+                print("pressed " + key);
+                aplpha = 255;
+            }
+            else
+            {
+                aplpha = aplpha - .5;
+            }
+        }
+
+        text.color = new Color32(255, 255, 255, (byte) aplpha);
+    }
+
+    IEnumerator FadeIn()
+    {
+        yield return new WaitForSeconds(delay);
+    }
+}
